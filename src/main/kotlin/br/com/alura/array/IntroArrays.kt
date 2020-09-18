b0ca4df089fca1e76b471ec2bd1db98ec5b47eee
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-09-18 20:36:17 -0300

    Achar maior idade com array e for no lugar de condicionais.

--- src/main/kotlin/br/com/alura/array/IntroArrays.kt
+++ src/main/kotlin/br/com/alura/array/IntroArrays.kt
@@ -1,20 +1,13 @@
 package br.com.alura.array
 
 fun main() {
-    val idades = IntArray(4)
-    idades[0] = 25
-    idades[1] = 19
-    idades[2] = 33
-    idades[3] = 20
+    val idades: IntArray = intArrayOf(25, 19, 33, 20)
 
-    val maiorIdade = if (idades[0] > idades[1] && idades[0] > idades[2] && idades[0] > idades[3]) {
-        idades[0]
-    } else if (idades[1] > idades[2] && idades[1] > idades[3]) {
-        idades[1]
-    } else if (idades[2] > idades[3]) {
-        idades[2]
-    } else {
-        idades[3]
+    var maiorIdade = Int.MIN_VALUE
+    for (idade in idades) {
+        if(maiorIdade < idade){
+            maiorIdade = idade
+        }
     }
 
     println(maiorIdade)
