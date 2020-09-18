a27623d69c1a36340bb9b949d8f5bf085567793d
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-09-18 20:52:13 -0300

    Aumento de salario usando for e indices.

--- src/main/kotlin/br/com/alura/array/IntroArrays.kt
+++ src/main/kotlin/br/com/alura/array/IntroArrays.kt
@@ -1,11 +1,19 @@
 package br.com.alura.array
 
 fun main() {
+
+    acharMaiorEMenorIdade()
+
+    processarAumentoDeSalario()
+
+}
+
+private fun acharMaiorEMenorIdade() {
     val idades: IntArray = intArrayOf(25, 19, 33, 20, 55, 40)
 
     var maiorIdade = Int.MIN_VALUE
     for (idade in idades) {
-        if(maiorIdade < idade){
+        if (maiorIdade < idade) {
             maiorIdade = idade
         }
     }
@@ -14,10 +22,23 @@ fun main() {
 
     var menorIdade = Int.MAX_VALUE
     idades.forEach { idade ->
-        if(idade < menorIdade) {
+        if (idade < menorIdade) {
             menorIdade = idade
         }
     }
 
     println(menorIdade)
+}
+
+private fun processarAumentoDeSalario() {
+    val salarios: DoubleArray = doubleArrayOf(1500.50, 2300.0, 5000.0, 8000.0, 10000.0)
+
+    println(salarios.contentToString())
+
+    val aumento = 1.1
+    for (indice in salarios.indices) {
+        salarios[indice] = salarios[indice] * aumento
+    }
+
+    println(salarios.contentToString())
 }
