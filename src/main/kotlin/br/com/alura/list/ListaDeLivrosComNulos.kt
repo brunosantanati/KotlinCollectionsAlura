f4c35ae66648d851abe566171874d34e87382484
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-09-30 16:48:16 -0300

    Usando groupBy e Elvis Operator.

--- src/main/kotlin/br/com/alura/list/ListaDeLivrosComNulos.kt
+++ src/main/kotlin/br/com/alura/list/ListaDeLivrosComNulos.kt
@@ -1,5 +1,11 @@
 package br.com.alura.list
 
 fun main() {
-    listaLivrosComNulos.imprimeComMarcadores()
+    //listaLivrosComNulos.imprimeComMarcadores()
+
+    listaDeLivros
+        .groupBy { it.editora ?: "Editora desconhecida" }
+        .forEach { (editora: String, livros: List<Livro>) ->
+            println("$editora: ${livros.joinToString { it.titulo }}")
+        }
 }
