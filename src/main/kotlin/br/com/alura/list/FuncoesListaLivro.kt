c7302fc7216dc9bb7842cb8d3641ebaecb1091fe
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-09-30 14:21:27 -0300

    Usando filterNotNull para remover nulos da lista.

--- src/main/kotlin/br/com/alura/list/FuncoesListaLivro.kt
+++ src/main/kotlin/br/com/alura/list/FuncoesListaLivro.kt
@@ -1,8 +1,11 @@
 package br.com.alura.list
 
 fun List<Livro?>.imprimeComMarcadores(){
-    val textoFormatado = this.joinToString(separator = "\n") {
-        " - ${it?.titulo} de ${it?.autor} - ${it?.anoPublicacao}"
+    val textoFormatado = this
+        //.filter { it != null }
+        .filterNotNull()
+        .joinToString(separator = "\n") {
+        " - ${it.titulo} de ${it.autor} - ${it.anoPublicacao}"
     }
     println(" #### Lista de Livros #### \n$textoFormatado")
 }
